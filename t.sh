729d40086288b9937e97cb8fc6b753d57ddf87fe
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2022-11-07 00:25:19 +0800

    demo key

--- t.sh
+++ t.sh
@@ -4,4 +4,4 @@
 # https://github.com/semaphore-protocol/semaphore/blob/main/packages/contracts/deployed-contracts/goerli.json
 # 0xe0A452533853310C371b50Bd91BB9DCC8961350F
 
-time forge test --fork-url https://eth-goerli.g.alchemy.com/v2/EjoWz0VskKdQvqvMrB-IDGtpI5t0m2Bw --fork-block-number 7888655 --libraries=src/Hashes.sol:PoseidonT3:0xe0A452533853310C371b50Bd91BB9DCC8961350F -vv
+time forge test --fork-url https://eth-goerli.g.alchemy.com/v2/demo --fork-block-number 7888655 --libraries=src/Hashes.sol:PoseidonT3:0xe0A452533853310C371b50Bd91BB9DCC8961350F -vv
